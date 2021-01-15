3cc49f53830b37128cae44d58e26e5a02304208a
josh <josh.fried13@gmail.com>
2021-01-15 18:39:18 -0500

    htne2021: backend 2021 init commit

---
+++ helpthehomeless/src/main/java/com/htne/helpthehomeless/HelpthehomelessApplication.java
@@ -0,0 +1,13 @@
+package com.htne.helpthehomeless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpthehomelessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelpthehomelessApplication.class, args);
+    }
+
+}
