9190b0b45ff56dbae20efa86df1821660551dac0
Hamza Kaced <hamzayoucefkaced@gmail.com>
2021-01-16 16:21:26 -0500

    Get Closest shelters details

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: java -cp helpthehomeless/target/helpthehomeless-0.0.1-SNAPSHOT.jar HelpthehomelessApplication
--- helpthehomeless/src/main/java/com/htne/helpthehomeless/dal/service/ShelterService.java
+++ helpthehomeless/src/main/java/com/htne/helpthehomeless/dal/service/ShelterService.java
@@ -10,9 +10,13 @@ import com.htne.helpthehomeless.dal.model.User;
 import com.htne.helpthehomeless.dal.service.exceptions.ExceptionHelper;
 import com.htne.helpthehomeless.dal.service.exceptions.HTNENotFoundException;
 import com.htne.helpthehomeless.dto.ShelterDTO;
+import com.jayway.jsonpath.spi.json.GsonJsonProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -54,6 +58,40 @@ public class ShelterService {
     public ShelterDTO updateShelter(final ShelterDTO dto) {
         shelterRepository.save(ShelterMapper.updateFields(dto, fetchShelter(dto.getId())));
         return dto;
+    }
+
+
+    public String getRegisteredSheltersWithinRadius(double longitude, double latitude, int radius){
+        List<Shelter> registeredShelterList = new ArrayList<>();
+
+        for(Shelter shelter : shelterRepository.findAll()){
+            if(distance(shelter.getLocation().getLatitude(), shelter.getLocation().getLongitude(), latitude, longitude,
+                    "K") <= radius/1000) {
+                registeredShelterList.add(shelter);
+            };
+        }
+
+        String registeredShelterJSON = new GsonJsonProvider().toJson(registeredShelterList);
+        return registeredShelterJSON;
+    }
+
+    private static double distance(double lat1, double lon1, double lat2, double lon2, String unit) {
+        if ((lat1 == lat2) && (lon1 == lon2)) {
+            return 0;
+        }
+        else {
+            double theta = lon1 - lon2;
+            double dist = Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2)) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(theta));
+            dist = Math.acos(dist);
+            dist = Math.toDegrees(dist);
+            dist = dist * 60 * 1.1515;
+            if (unit.equals("K")) {
+                dist = dist * 1.609344;
+            } else if (unit.equals("N")) {
+                dist = dist * 0.8684;
+            }
+            return (dist);
+        }
     }
 
 }
