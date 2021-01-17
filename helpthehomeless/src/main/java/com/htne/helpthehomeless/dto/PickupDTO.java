ac8637a75ee36a16370781f61360e2d49700062f
josh <josh.fried13@gmail.com>
2021-01-17 13:16:09 -0500

    bugfixing

--- helpthehomeless/src/main/java/com/htne/helpthehomeless/dto/PickupDTO.java
+++ helpthehomeless/src/main/java/com/htne/helpthehomeless/dto/PickupDTO.java
@@ -1,8 +1,10 @@
 package com.htne.helpthehomeless.dto;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.htne.helpthehomeless.dal.model.Reservation;
 import com.htne.helpthehomeless.dal.model.User;
 import lombok.*;
+
 import java.util.Date;
 
 @AllArgsConstructor
@@ -10,6 +12,7 @@ import java.util.Date;
 @Getter
 @Setter
 @EqualsAndHashCode
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.WRAPPER_OBJECT)
 public class PickupDTO {
     private Long id;
     private Reservation reservation;
--- helpthehomeless/src/main/java/com/htne/helpthehomeless/dto/VisitDTO.java
+++ helpthehomeless/src/main/java/com/htne/helpthehomeless/dto/VisitDTO.java
@@ -1,5 +1,6 @@
 package com.htne.helpthehomeless.dto;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.*;
 
 import java.util.Date;
@@ -9,6 +10,7 @@ import java.util.Date;
 @Getter
 @Setter
 @EqualsAndHashCode
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.WRAPPER_OBJECT)
 public class VisitDTO {
     private Long       id;
     private UserDTO    user;
